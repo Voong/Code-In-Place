d0d3f1d6b7b20b0e2855692bb88379e36e7c5022
[Anthony Voong] <[voong41@gmail.com]>
2021-07-04 20:55:49 -0400

    Added the player class

--- project.py
+++ project.py
@@ -22,6 +22,44 @@ SCREEN_HEIGHT = 800
 res = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(res)
 
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+
+        super(Player, self).__init__()
+
+        self.surf = pygame.image.load(r'H:\CodeInPlace\FinalProject\karel.png').convert()
+        self.surf.set_colorkey(white, RLEACCEL)
+        self.surf = pygame.transform.scale(self.surf, (50,50))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -20)
+            #move_up_sound.play()
+
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 20)
+            #move_down_sound.play()
+
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-20, 0)
+
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(20, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
 
 
 if __name__ == '__main__':
